ad4fb039e01ec750cb85b9c749bafa0daf91a5a8
Ваше Имя <you@example.com>
2020-10-29 20:42:01 +0600

    oxo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
+import {Container, Nav, Navbar} from "react-bootstrap";
+
 
 class Header extends Component {
     render() {
         return (
             <header>
-                <nav>
-                    <ul>
-                        <li>Главная</li>
-                        <li>Товары</li>
-                        <li>О нас</li>
-                        <li>Контакты</li>
-                    </ul>
-                </nav>
+                <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav>
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#products">Products</Nav.Link>
+                                <Nav.Link href="#about">About</Nav.Link>
+                                <Nav.Link href="#contact">Contact</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
             </header>
         );
     }
