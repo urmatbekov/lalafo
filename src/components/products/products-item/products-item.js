a18cec44b4d1c0389b5bb072b53e646e1a3f61fa
mr-oroz <oroz7307@gmail.com>
2020-10-31 19:11:43 +0600

    state

--- src/components/products/products-item/products-item.js
+++ src/components/products/products-item/products-item.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import "./products-item.css"
 
 class ProductsItem extends Component {
+    state = {
+        image: '/img/televizor.jpg',
+        price: 200,
+        title: "Televizor"
+    }
     render() {
+        const {title, image, price}= this.state;
         return (
             <div className="col-md-4">
                 <li className="product-item">
-                    <h3>{this.props.title}</h3>
-                    <img src={this.props.image} alt={""}/>
-                    <p>Price: <span>200$</span>
+                    <h3>{title}</h3>
+                    <img src={image} alt={""}/>
+                    <p>Price: <span>{price}$</span>
                         <button className="btn btn-danger buy-button">Купить</button>
                     </p>
                 </li>
