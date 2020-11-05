a40ec89bf68bf10ee9b550709bab8531af45be41
Ваше Имя <you@example.com>
2020-11-05 19:42:00 +0600

    add

--- src/components/data-container/data-container.js
+++ src/components/data-container/data-container.js
@@ -3,20 +3,20 @@ import {DataProvider} from "./provider";
 
 class DataContainer extends Component {
     state = {
-        data:[]
+        data: []
     }
     startId = 100;
     addProduct = (item) => {
         this.startId += 1
         this.setState(({data}) => {
-            return {data: [...data, {...item, id: this.startId, image: '/img/televizor.jpg',}]}
+            return {data: [...data, {...item}]}
         })
     }
 
     deleteProduct = (id) => {
         this.setState(({data}) => {
             const index = data.findIndex((item) => item.id === id)
-            if (index === -1){
+            if (index === -1) {
                 return {}
             }
             return {data: [...data.slice(0, index), ...data.slice(index + 1)]}
@@ -24,10 +24,10 @@ class DataContainer extends Component {
     }
 
     componentDidMount() {
-        fetch('https://nurkadyr.pythonanywhere.com/product/').then((res)=>{
+        fetch('https://nurkadyr.pythonanywhere.com/product/').then((res) => {
             return res.json()
-        }).then((data)=>{
-            this.setState({data:data.results})
+        }).then((data) => {
+            this.setState({data: data.results})
         })
     }
 
