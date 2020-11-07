ec95bd9d8832036bc1353be00e5615c96541b1df
Ваше Имя <you@example.com>
2020-11-07 20:56:51 +0600

    add

--- src/components/product-detail/product-detail.js
+++ src/components/product-detail/product-detail.js
@@ -1,29 +1,21 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
-import {DataConsumer} from "../data-container/provider";
+import {connect} from "react-redux";
 
 class ProductDetail extends Component {
     render() {
         const id = +this.props.match.params.id
+        const item = this.props.data.find((item) => item.id === id)
         return (
             <div className="container">
-                <DataConsumer>
-                    {
-                        ({data}) => {
-                            const item = data.find((item) => item.id === id)
-                            return (
-                                <div>
-                                    <h1>{item.title}</h1>
-                                    <img style={{width:'400px'}} src={item.image} alt=""/>
-                                    <p>{item.price} $</p>
-                                </div>
-                            )
-                        }
-                    }
-                </DataConsumer>
+                <div>
+                    <h1>{item.title}</h1>
+                    <img style={{width: '400px'}} src={item.image} alt=""/>
+                    <p>{item.price} $</p>
+                </div>
             </div>
         );
     }
 }
 
-export default withRouter(ProductDetail);
+export default withRouter(connect(({data})=>({data}))(ProductDetail));
