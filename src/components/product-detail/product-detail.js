fd283e46dc05127177ab7e7eaf34c5d9b63862ca
abibilla <abibilla1>
2020-12-01 19:49:57 +0600

    ewu

--- src/components/product-detail/product-detail.js
+++ src/components/product-detail/product-detail.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
-<<<<<<< HEAD
 import {connect} from "react-redux";
-=======
 
->>>>>>> mr-oroz
 
 class ProductDetail extends Component {
     render() {
@@ -12,29 +9,26 @@ class ProductDetail extends Component {
         const item = this.props.data.find((item) => item.id === id)
         return (
             <div className="container">
-<<<<<<< HEAD
                 <div>
                     <h1>{item.title}</h1>
                     <img style={{width: '400px'}} src={item.image} alt=""/>
                     <p>{item.price} $</p>
                 </div>
-=======
-                    {
-                        ({data}) => {
-                            const item = data.find((item) => item.id === id)
-                            return (
-                                <div>
-                                    <h1>{item.title}</h1>
-                                    <img style={{width:'400px'}} src={item.image} alt=""/>
-                                    <p>{item.price} $</p>
-                                </div>
-                            )
-                        }
+                {
+                    ({data}) => {
+                        const item = data.find((item) => item.id === id)
+                        return (
+                            <div>
+                                <h1>{item.title}</h1>
+                                <img style={{width: '400px'}} src={item.image} alt=""/>
+                                <p>{item.price} $</p>
+                            </div>
+                        )
                     }
->>>>>>> mr-oroz
+                }
             </div>
         );
     }
 }
 
-export default withRouter(connect(({data})=>({data}))(ProductDetail));
+export default withRouter(connect(({data}) => ({data}))(ProductDetail));
