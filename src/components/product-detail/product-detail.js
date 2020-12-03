721991dc9a3195e203867491b9fe4692d7272f1c
mr-oroz <oroz7307@gmail.com>
2020-12-03 19:17:20 +0600

    add

--- src/components/product-detail/product-detail.js
+++ src/components/product-detail/product-detail.js
@@ -1,24 +1,18 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
-<<<<<<< HEAD
-
-=======
->>>>>>> 630a8d845fbcd1b281c5887cdbe46eee24652949
 
 class ProductDetail extends Component {
     render() {
         const id = +this.props.match.params.id
         return (
             <div className="container">
-<<<<<<< HEAD
+
                 <div>
                     <h1>{item.title}</h1>
                     <img style={{width: '400px'}} src={item.image} alt=""/>
                     <p>{item.price} $</p>
                 </div>
-=======
->>>>>>> 630a8d845fbcd1b281c5887cdbe46eee24652949
                 {
                     ({data}) => {
                         const item = data.find((item) => item.id === id)
@@ -31,10 +25,6 @@ class ProductDetail extends Component {
                         )
                     }
                 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 630a8d845fbcd1b281c5887cdbe46eee24652949
             </div>
         );
     }
